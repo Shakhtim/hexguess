08e12cd37e2a3c0fa605526019eccf0316374d5b
Шахбанов Тамирлан <t.shakhbanov@yandex.ru>
2026-02-05 16:07:56 +0300

    Возврат к стандартному конфигу - base: './', assets/, шрифты

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '', // Empty base for Yandex.Games
+  base: './',
   build: {
-    assetsDir: '',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'index.js',
-        chunkFileNames: 'index.js',
-        assetFileNames: 'index.[ext]'
-      }
-    }
+    assetsDir: 'assets',
   }
 })
